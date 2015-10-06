e4a73a5034c6f98545533c4be62b29c1b9680a1f
smjrifle <smjrifle@gmail.com>
2015-10-06 13:07:35 +0545

    Removed cache

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'bedilovestohack');
+define('DB_NAME', 'srijanalaya');
 
 /** MySQL database username */
 define('DB_USER', 'root');
