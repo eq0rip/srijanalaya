b349e6e84ff63cd2b79f3e0e7c7802315bf5cc4a
smjrifle <smjrifle@gmail.com>
2015-10-15 12:17:30 +0545

    Slider Width

--- wp-content/themes/srijanalaya/js/custom.js
+++ wp-content/themes/srijanalaya/js/custom.js
@@ -2,7 +2,7 @@
 
 (function($) {
   $(window).load(function(){
-$('.loader').fadeOut(500);
+    $('.loader').fadeOut(500);
   });
   $(document).ready(function($){
 
@@ -16,9 +16,9 @@ var $owl1 = $("#slider-1"),
 $owl2 = $("#slider-2"),
 flag = false,
 duration = 300;
- var bar=2;
- var car ='#bar1';
-  $('#bar1').css({'width':'100%','height':'100%'});
+var bar=2;
+var car ='#bar1';
+$('#bar1').css({'width':'100%','height':'100%'});
 $owl1
 .owlCarousel({
   items: 1,
@@ -28,12 +28,12 @@ $owl1
   loop:true
 })
 .on('changed.owl.carousel', function (e) {
-    $(car).css({'width':'0px','height':'0px'});
-    if(bar==4)
-      bar=1;
-    car='#bar'+bar;
-    $(car).css({'width':'100%','height':'100%'});
-    bar++;
+  $(car).css({'width':'0px','height':'0px'});
+  if(bar==4)
+    bar=1;
+  car='#bar'+bar;
+  $(car).css({'width':'100%','height':'100%'});
+  bar++;
   if (!flag) {
     flag = true;
 
@@ -60,23 +60,23 @@ $owl2
 //====owl sync end
 
 //nav icons
-  $('.nav-icons').hover(function() {
-    $('.nav-links').stop(true,true).slideDown(100);
-  }, function() {
-    $('.nav-links').stop(true,true).slideUp(100);
-  }
-  );
+$('.nav-icons').hover(function() {
+  $('.nav-links').stop(true,true).slideDown(100);
+}, function() {
+  $('.nav-links').stop(true,true).slideUp(100);
+}
+);
 
 
 $(".dropdown").hover(            
   function() {
     if($(window).width() > 769)
       $(this).children('ul').slideDown(200);       
-             },
-             function() {
-              if($(window).width() > 769)
-                $(this).children('ul').fadeOut(100);               
-              });
+  },
+  function() {
+    if($(window).width() > 769)
+      $(this).children('ul').fadeOut(100);               
+  });
 
 
 $("#right-col-first").hover(
