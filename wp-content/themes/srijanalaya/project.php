0a1faa413807f2a966448b36dcdf3716630fd37a
smjrifle <smjrifle@gmail.com>
2015-10-16 15:45:24 +0545

    Timeline Fixes

--- wp-content/themes/srijanalaya/project.php
+++ wp-content/themes/srijanalaya/project.php
@@ -6,25 +6,52 @@ Template Name: Timeline
 get_header(); ?>
 <section id="cd-timeline" class="cd-container">
 	<?php
-	$args=array('posts_per_page'=>20, 'post_type'=>'project', 'meta_key' => 'wpcf-date', 'orderby' => 'meta_value', 'order' => 'DESC'); 
-$postslist=new WP_Query($args);                          //
-while($postslist->have_posts()) : $postslist->the_post();//
-?>
+	$nextEvent = '';
+	$args=array('posts_per_page'=>1, 'post_type'=>'project','meta_key' => 'wpcf-project-date',
+		'meta_query' => array(
+			array(
+				'key' => 'wpcf-project-date'
+				),
+			array(
+				'key' => 'wpcf-project-date',
+				'value' => strtotime('today'),
+				'compare' => '>='
+				)
+			),
+		'orderby' => 'meta_value',
+		'order' => 'ASC'
+		); 
+	$postslist=new WP_Query($args);
+	while($postslist->have_posts()) : $postslist->the_post();
+	$nextEvent = $post->ID;
+	endwhile;
+	$args=array('posts_per_page'=>20, 'post_type'=>'project', 'meta_key' => 'wpcf-project-date', 'orderby' => 'meta_value', 'order' => 'DESC'); 
+	$postslist=new WP_Query($args);              
+	$curDate = date('now');
+	while($postslist->have_posts()) : $postslist->the_post();
+	$date = types_render_field('project-date');
+	?>
 
-<div class="cd-timeline-block">
-	<div class="cd-timeline-img cd-picture">
-		<img src="img/cd-icon-picture.svg" alt="Picture">
-	</div> <!-- cd-timeline-img -->
+	<div class="cd-timeline-block" <?php if($post->ID == $nextEvent) echo 'id="next"';?>>
+		<div class="cd-timeline-img cd-picture <?php if($post->ID == $nextEvent) echo 'next-project';?>">
+			<?php echo date('d', strtotime($date)) . $diff;?>
+		</div> <!-- cd-timeline-img -->
 
-	<div class="cd-timeline-content">
-	<h2><?php the_title();?></h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-		<a href="#0" class="cd-read-more">Read more</a>
-		<span class="cd-date">Jan 14</span>
-	</div> <!-- cd-timeline-content -->
-</div> <!-- cd-timeline-block -->
+		<div class="cd-timeline-content">
+			<h2><?php the_title();?></h2>
+			<p><?php echo types_render_field('summary');?></p>
+			<a href="<?php the_permalink();?>" class="cd-read-more">Read more</a>
+			<span class="cd-date"><?php echo $date;?></span>
+		</div> <!-- cd-timeline-content -->
+	</div> <!-- cd-timeline-block -->
 <?php endwhile; ?>
 </section> <!-- cd-timeline -->
 <?php get_footer(); ?>
 <script src="<?php echo get_template_directory_uri();?>/js/mordenizer.js"></script> <!-- Modernizr -->
 <script src="<?php echo get_template_directory_uri();?>/js/timeline.js"></script> <!-- Resource jQuery -->
+<script type="text/javascript">
+	jQuery(window).load(function() {
+		var winSize = (jQuery(window).height()) / 2;
+		jQuery("html, body").animate({scrollTop: (jQuery('#next').offset().top - winSize) }, 1000);
+	});
+</script>
