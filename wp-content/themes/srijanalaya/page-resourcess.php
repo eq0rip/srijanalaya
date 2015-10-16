bbe839e3625efea4126742075b315b6591e34456
eq0rip <lstrrt@gmail.com>
2015-10-16 17:09:22 +0545

    sfd

--- wp-content/themes/srijanalaya/page-resourcess.php
+++ wp-content/themes/srijanalaya/page-resourcess.php
@@ -4,4 +4,12 @@ Template Name: resource
  */
 
 get_header(); ?>
+</div>
+<div class="container-fluid">
+	<div class="topbanner">
+		<h1>Featured Resource</h1>
+		<h2>Promotion Phare</h2>
+	</div>
+
+
 <?php get_footer(); ?>
