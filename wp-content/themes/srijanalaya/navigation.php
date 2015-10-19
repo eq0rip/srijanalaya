baa7d5f603e5553e4109e0ff2a968a42f469c7d0
smjrifle <smjrifle@gmail.com>
2015-10-19 12:13:03 +0545

    Map Changes

--- wp-content/themes/srijanalaya/navigation.php
+++ wp-content/themes/srijanalaya/navigation.php
@@ -1,20 +1,20 @@
 <?php if(( is_home() || is_front_page() )) { 
   $class = 'hideme';
-  }
-  else {
-    $class = '';
-    }
-    ?>
+}
+else {
+  $class = '';
+}
+?>
 
-      <nav class="nav-icons fixed <?php echo $class;?>">
-        <ul class="nav-links" style="display:none;">
-          <li class="message"><a href="<?php echo site_url();?>/creatives"><span>Contact</span></a></li>
-          <li class="bag"><a href="<?php echo site_url();?>/about"><span>Shop</span></a></li>
-          <li class="bag"><a href="<?php echo site_url();?>/service"><span>Resources</span></a></li>
-          <li class="message"><a href="<?php echo site_url();?>/contact"><span>Projects</span></a></li>
-          <li class="pencil"><a href="#!"><span>News</span></a></li>
-        </ul>
-        <ul>
-          <li class="plus"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/icon-plus.png"><span></span></a></li>
-        </ul>
-      </nav>
+<nav class="nav-icons fixed <?php echo $class;?>">
+  <ul class="nav-links" style="display:none;">
+    <li class="message"><a href="<?php echo site_url();?>/creatives"><span>Contact</span></a></li>
+    <li class="bag"><a href="<?php echo site_url();?>/about"><span>Shop</span></a></li>
+    <li class="bag"><a href="<?php echo site_url();?>/service"><span>Resources</span></a></li>
+    <li class="message"><a href="<?php echo site_url();?>/contact"><span>Projects</span></a></li>
+    <li class="pencil"><a href="#!"><span>News</span></a></li>
+  </ul>
+  <ul>
+    <li class="plus"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/icon-plus.png"><span></span></a></li>
+  </ul>
+</nav>
