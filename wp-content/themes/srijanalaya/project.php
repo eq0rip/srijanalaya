27af97464f853e62ab5a3457c66f1e9d914fc74d
smjrifle <smjrifle@gmail.com>
2015-10-25 14:57:24 +0545

    timeline css

--- wp-content/themes/srijanalaya/project.php
+++ wp-content/themes/srijanalaya/project.php
@@ -29,6 +29,7 @@ get_header(); ?>
 		$args=array('posts_per_page'=>20, 'post_type'=>'project', 'meta_key' => 'wpcf-project-date', 'orderby' => 'meta_value', 'order' => 'DESC'); 
 		$postslist=new WP_Query($args);              
 		$curDate = date('now');
+		$i = 0;
 		while($postslist->have_posts()) : $postslist->the_post();
 		$date = date('m-y-d',types_render_field('project-date', array('raw' => 'true')));
 		$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'large');
@@ -36,6 +37,14 @@ get_header(); ?>
 			$image = '';
 		else
 			$image = $imgsrc[0];
+		if($i == 0) {
+			$class = 'project-wrapper-left';
+			$i++;
+		}
+		else {
+			$class = 'project-wrapper-right';
+			$i=0;
+		}
 		?>
 
 		<div class="cd-timeline-block" <?php if($post->ID == $nextEvent) echo 'id="next"';?>>
@@ -43,11 +52,14 @@ get_header(); ?>
 				<span><?php echo parseDate($date);?></span>
 			</div> <!-- cd-timeline-img -->
 			<div class="cd-timeline-content">
-			<div class="wrapper"><?php echo "<img src='" . $image . "' alt='" . get_the_title() . "' />"; ?></div>
-				<h2><?php the_title();?></h2>
-				<p><?php echo types_render_field('summary');?></p>
-				<a href="<?php the_permalink();?>" class="cd-read-more">Read more</a>
-			</div> <!-- cd-timeline-content -->
+				<div class="project-wrapper <?php echo $class;?>" <?php echo "style = 'background-image: url(http://localhost/srijanalaya/wp-content/uploads/2015/10/Srijanalaya_projects_2.png);'";?>>
+					<div class="content">
+						<h2><?php the_title();?></h2>
+						<p><?php echo types_render_field('summary');?></p>
+						<a href="<?php the_permalink();?>" class="cd-read-more">Read more</a>
+					</div>
+				</div> <!-- cd-timeline-content -->
+			</div>
 		</div> <!-- cd-timeline-block -->
 	<?php endwhile; ?>
 </section> <!-- cd-timeline -->
