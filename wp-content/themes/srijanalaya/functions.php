1544a970ca278504ca847268dc9ccc611a55622f
smjrifle <smjrifle@gmail.com>
2015-11-04 11:52:18 +0545

    Latest oush

--- wp-content/themes/srijanalaya/functions.php
+++ wp-content/themes/srijanalaya/functions.php
@@ -419,3 +419,32 @@ function wpse_hide_admin_bar() {
 	return false;
 }
 add_filter( 'show_admin_bar', 'wpse_hide_admin_bar' );
+
+
+//For post View
+
+function wpb_set_post_views($postID) {
+	$count_key = 'wpb_post_views_count';
+	$count = get_post_meta($postID, $count_key, true);
+	if($count==''){
+		$count = 0;
+		delete_post_meta($postID, $count_key);
+		add_post_meta($postID, $count_key, '0');
+	}else{
+		$count++;
+		update_post_meta($postID, $count_key, $count);
+	}
+}
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+function wpb_track_post_views ($post_id) {
+	if ( !is_single() ) return;
+	if ( empty ( $post_id) ) {
+		global $post;
+		$post_id = $post->ID;    
+	}
+	wpb_set_post_views($post_id);
+}
+add_action( 'wp_head', 'wpb_track_post_views');
+
+
