21122969308bdccf9d9722c0ac1a4170849bfb80
smjrifle <smjrifle@gmail.com>
2016-02-06 09:33:39 +0545

    Google Analytics

--- wp-content/themes/srijanalaya/functions.php
+++ wp-content/themes/srijanalaya/functions.php
@@ -616,3 +616,19 @@ function lower_wpseo_priority( $html ) {
     return 'low';
 }
 add_filter( 'wpseo_metabox_prio', 'lower_wpseo_priority' );
+
+
+<?php
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73405651-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php } ?>
